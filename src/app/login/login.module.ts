9914ef0905880b9505c7cb255fe53952546f250b
Lucas Soares <lucasps6@hotmail.com>
2021-08-21 13:35:15 -0300

    Criado o LoginComponent

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { LoginComponent } from './loginComponent';
+
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LoginComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    LoginComponent
   ]
 })
 export class LoginModule { }
