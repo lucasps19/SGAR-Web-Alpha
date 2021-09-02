0085ef166abfb08f1269182291fcf23eba7b8816
Lucas Soares <lucasps6@hotmail.com>
2021-09-01 22:59:30 -0300

    Criado o app-routing da aplicacao

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModules } from './app-routing.modules';
 import { LoginModule } from './login/Index';
 
 @NgModule({
@@ -9,6 +10,7 @@ import { LoginModule } from './login/Index';
     AppComponent
   ],
   imports: [
+    AppRoutingModules,
     BrowserModule,
     LoginModule
   ],
