4479e5c7c4ac3f338c5040ad0c8c868298e1ffcb
Lucas Soares <lucasps6@hotmail.com>
2021-09-18 09:35:03 -0300

    Iniciando a criacao dos componentes de cadastro

--- src/app/login/cadastrar/cadastrar-usuario.component.ts
+++ src/app/login/cadastrar/cadastrar-usuario.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Pessoa } from '../shared';
 
 @Component({
   selector: 'app-cadastrar-usuario',
@@ -7,9 +10,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastrarUsuarioComponent implements OnInit {
 
-  constructor() { }
+  public pessoa = new Pessoa();
+  public formularioCadastroUsuario: FormGroup;
+
+  constructor(
+    //protected loginService: LoginService,
+    protected router: Router,
+    protected formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.criarFormulario();
+  }
+
+  protected criarFormulario() {
+    this.formularioCadastroUsuario = this.formBuilder.group({
+      nome: [this.pessoa.nome],
+      cpf: [this.pessoa.cpf],
+      email: [this.pessoa.email]
+      //senha: [this.pessoa.senha]
+    });
   }
 
 }
