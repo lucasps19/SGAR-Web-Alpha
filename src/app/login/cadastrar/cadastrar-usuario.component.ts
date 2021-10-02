32751ad69631e832598b0642fffacf070f664e0d
Lucas Soares <lucasps6@hotmail.com>
2021-10-02 18:34:59 -0300

    Criado validacao de campos preenchidos

--- src/app/login/cadastrar/cadastrar-usuario.component.ts
+++ src/app/login/cadastrar/cadastrar-usuario.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Empresa, LoginService, Pessoa } from '../shared';
 
@@ -26,33 +26,40 @@ export class CadastrarUsuarioComponent implements OnInit {
 
   protected criarFormulario() {
     this.formularioCadastroUsuario = this.formBuilder.group({
-      nome: [this.pessoa.nome],
-      cpf: [this.pessoa.cpf],
-      email: [this.pessoa.email],
-      empresa: [this.pessoa.idEmpresa],
-      senha: [this.pessoa.senha]
+      nome: [this.pessoa.nome, Validators.required],
+      cpf: [this.pessoa.cpf, Validators.required],
+      email: [this.pessoa.email, Validators.required],
+      empresa: [this.pessoa.idEmpresa, Validators.required],
+      senha: [this.pessoa.senha, Validators.required]
     });
   }
 
-  protected buscarEmpresasCadastradas(){
+  protected buscarEmpresasCadastradas() {
     this.loginService.buscarEmpresasCadastradas().then(dados => {
       this.listaEmpresasCadastradas = dados;
     })
   }
 
-  public cadastrarUsuario(){
-    this.pessoa.cpf = this.pessoa.cpf.replace(".", "").replace(".", "").replace("-", "");
-    debugger;
-    this.loginService.cadastrarUsuario(this.pessoa).subscribe(
-      response => {
-        console.log(response);
-        alert("Cadastro realizado com Sucesso");
-      },
-      error => {
-        console.log(error);
-        alert("Erro!");
-      }
-    )
+  public cadastrarUsuario() {
+    if (this.formularioCadastroUsuario.valid) {
+      this.pessoa.cpf = this.pessoa.cpf.replace(".", "").replace(".", "").replace("-", "");
+
+      this.loginService.cadastrarUsuario(this.pessoa).subscribe(
+        response => {
+          console.log(response);
+          alert("Cadastro realizado com Sucesso");
+        },
+        error => {
+          console.log(error);
+          alert("Erro!");
+        }
+      )
+    }else{
+      Object.keys(this.formularioCadastroUsuario.controls).forEach(campo => {
+        const controle = this.formularioCadastroUsuario.get(campo);
+        controle.markAsDirty();
+      });
+    }
   }
 
 }
