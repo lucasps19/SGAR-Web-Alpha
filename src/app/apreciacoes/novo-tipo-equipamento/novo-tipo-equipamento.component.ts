715cc4f1665c6f2687576f9ec7773377c57fb508
Lucas Soares <lucasps6@hotmail.com>
2021-11-02 16:54:59 -0300

    Criado tela de novo tipo equipamento

--- src/app/apreciacoes/novo-tipo-equipamento/novo-tipo-equipamento.component.ts
+++ src/app/apreciacoes/novo-tipo-equipamento/novo-tipo-equipamento.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { DynamicDialogRef } from 'primeng/dynamicdialog';
+import { TipoEquipamento } from '..';
 
 @Component({
   selector: 'app-novo-tipo-equipamento',
@@ -7,9 +10,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NovoTipoEquipamentoComponent implements OnInit {
 
-  constructor() { }
+  public formularioNovoTipoEquipamento: FormGroup;
+  public tipoEquipamento = new TipoEquipamento();
+
+  constructor(
+    protected formBuilder: FormBuilder,
+    public ref: DynamicDialogRef
+  ) { }
 
   ngOnInit(): void {
+    this.criarFormulario();
+  }
+
+  protected criarFormulario() {
+    this.formularioNovoTipoEquipamento = this.formBuilder.group({
+      descricao: [this.tipoEquipamento.descricao, Validators.required]
+    });
+  }
+
+  public Cancelar(){
+    this.ref.close();
+  }
+
+  public CadastrarTipoEquipamento() {
+    if (this.formularioNovoTipoEquipamento.valid) {
+
+    }else{
+      Object.keys(this.formularioNovoTipoEquipamento.controls).forEach(campo => {
+        const controle = this.formularioNovoTipoEquipamento.get(campo);
+        controle.markAsDirty();
+      });
+    }
   }
 
 }
